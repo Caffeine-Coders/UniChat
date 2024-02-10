b8401ae32abc3a46a3e11cd97462e828a016e3a0
ForumShah8 <shahforu00@gmail.com>
2024-02-10 18:15:39 -0500

    fixed login button hover

--- Project_SRC/src/app/components/navbar.js
+++ Project_SRC/src/app/components/navbar.js
@@ -2,11 +2,11 @@ export default function Nav() {
     return (
         <nav class="flex items-center justify-between flex-wrap p-6">
             <div class="flex items-center flex-shrink-0 mr-6">
-                <span class="font-bold text-4xl tracking-wide">UniChat</span>
+                <span class="font-bold text-2xl tracking-wide">UniChat</span>
             </div>
-            <div class="lg:flex lg:items-center">
+            <div class="flex items-center flex-shrink-0 mr-6">
                 <div>
-                    <a href="/login" class="inline-block text-xl px-4 py-2 leading-none rounded hover:border hover:border-slate-500 hover:border-4 ">Login</a>
+                    <a href="/login" class="inline-block px-4 py-2 leading-none border-2 border-transparent rounded hover:border hover:border-slate-500 hover:border-2">Login</a>
                 </div>
             </div>
         </nav>
