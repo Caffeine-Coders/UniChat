ddca196eef7bb4e34803363b40f33d17b6c16a73
Dheeraj Nani <dheerajnani@Dheerajs-Air.lan>
2024-02-10 16:33:42 -0500

    route

--- Project_SRC/src/app/page.js
+++ Project_SRC/src/app/page.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import Image from "next/image";
+import Link from "next/link";
+import Layout from './components/layout';
 
 export default function Home() {
   return (
+    <Layout>
     <h3>Welcome to UniChat</h3>
+    <h2><Link href="/login">Login</Link></h2>
+    </Layout>
   );
 }
