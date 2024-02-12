c10295c777375112140d3578b53c8addd04da3ba
ForumShah8 <shahforu00@gmail.com>
2024-02-12 00:04:02 -0500

    project dashboard students loaded

--- Project_SRC/src/app/prof/projectdetails/content.js
+++ Project_SRC/src/app/prof/projectdetails/content.js
@@ -1,12 +1,54 @@
 export default function Content(){
     return(
-        <div class="container justify-center flex items-center m-0">
-            <div class="ml-80 rounded-lg bg-blue-200 border w-full h-40 flex justify-center items-center">
-            <div class="font-bold mb-0  ">
-                        <h1 class=" mb-0  text-4xl">
-                            Project Name 1
-                        </h1>
-                    </div>
+        <div class="p-4 sm:ml-64">
+            <div class="p-4 border-2 border-gray-200  rounded-lg dark:border-gray-700">
+            <div class="flex items-center justify-center h-48 mb-4 rounded-lg bg-blue-200 ">
+                <h1 class="text-4xl font-extrabold text-gray-900 ">
+                    Project Name 1
+                </h1>
+            </div>
+            <div class="ml-28 mr-28  rounded-lg ">
+                <h1>
+                    Students
+                    <hr class="h-px  bg-gray-400 border-0"></hr>
+                </h1>
+            </div>
+            <div class="ml-28 mr-28  mt-4 rounded-lg flex justify-between space-between">
+                <h4 class="flex ">
+                <img class="ml-4 mr-4 w-6 h-6 rounded-full" src="https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg" alt=""/>
+                Person 1
+                </h4>
+                <h4 class="mr-4">somemeial@gmail.com</h4>
+                
+            </div>
+            <hr class="ml-28 mr-28  mt-4 h-px  bg-gray-200 border-0"></hr>
+            <div class="ml-28 mr-28  mt-4 rounded-lg flex justify-between space-between">
+                <h4 class="flex ">
+                <img class="ml-4 mr-4 w-6 h-6 rounded-full" src="https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg" alt=""/>
+                Person 1
+                </h4>
+                <h4 class="mr-4">somemeial@gmail.com</h4>
+                
+            </div>
+            <hr class="ml-28 mr-28  mt-4 h-px  bg-gray-200 border-0"></hr>
+            <div class="ml-28 mr-28  mt-4 rounded-lg flex justify-between space-between">
+                <h4 class="flex ">
+                <img class="ml-4 mr-4 w-6 h-6 rounded-full" src="https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg" alt=""/>
+                Person 1
+                </h4>
+                <h4 class="mr-4">somemeial@gmail.com</h4>
+                
+            </div>
+            <hr class="ml-28 mr-28  mt-4 h-px  bg-gray-200 border-0"></hr>
+            <div class="ml-28 mr-28  mt-4 rounded-lg flex justify-between space-between">
+                <h4 class="flex ">
+                <img class="ml-4 mr-4 w-6 h-6 rounded-full" src="https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg" alt=""/>
+                Person 1
+                </h4>
+                <h4 class="mr-4">somemeial@gmail.com</h4>
+                
+            </div>
+            <hr class="ml-28 mr-28  mt-4 h-px  bg-gray-200 border-0"></hr>
             </div>
         </div>
     )
