f6f14dd73985034fd0a9bb66e157b880e0ef8a22
ForumShah8 <shahforu00@gmail.com>
2024-02-12 00:54:56 -0500

    dashboard cards fixed

--- Project_SRC/src/app/prof/content.js
+++ Project_SRC/src/app/prof/content.js
@@ -3,26 +3,42 @@ export default function Content() {
 <>
         <div class="container my-12  mx-auto px-2 ">
     <div class="flex flex-wrap -mx-1 lg:-mx-4 lg:mx-20">
-        <div class="my-1 w-full md:w-1/2 lg:my-4  lg:px-8 lg:w-1/3 h-40">
-        <a class="no-underline  text-black h-full" href="/prof/newproject">
-            <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2 mt-6 flex justify-center items-center">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
-                        </svg>
+        <div class="my-1 w-full md:w-1/2 lg:my-4 pb-4 lg:px-6 lg:w-1/3 h-56">
+            <a class="no-underline  text-black h-full" href="/prof/newproject">
+                <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
+                    <div class="px-6 py-12">
+                        <div class="font-bold text-xl mb-2 mt-6 flex justify-center items-center">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
+                            </svg>
+                        </div>
+                        <h1 class="text-xl text-center hover:underline">
+                                Add Project
+                        </h1>
                     </div>
-                    <h1 class="text-xl text-center hover:underline">
-                            Add Project
-                    </h1>
-                </div>
-            </article>
+                </article>
             </a>
         </div>
-        <div class="my-1 w-full md:w-1/2 lg:my-4  lg:px-8 lg:w-1/3 h-40">
+        <div class="my-1 w-full md:w-1/2 lg:my-4 pb-4 lg:px-6 lg:w-1/3 h-56">
+            <a class="no-underline  text-black" href="/prof/projectdetails">
+                <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
+                    <div class="px-6 py-12">
+                        <div class="font-bold text-xl mb-0 mt-6 flex justify-center items-center">
+                            <h1 class="mt-2 mb-0 text-xl text-center hover:underline">
+                                Project Name 1
+                            </h1>
+                        </div>
+                        <h6 class="text-xs text-center my-0">    
+                                Grade Level
+                        </h6>
+                    </div>
+                </article>
+            </a>
+        </div>
+        <div class="my-1 w-full md:w-1/2 lg:my-4 pb-4 lg:px-6 lg:w-1/3 h-56">
         <a class="no-underline  text-black" href="/prof/projectdetails">
             <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
-                <div class="px-6 py-4">
+                <div class="px-6 py-12">
                     <div class="font-bold text-xl mb-0 mt-6 flex justify-center items-center">
                         <h1 class="mt-2 mb-0 text-xl text-center hover:underline">
                             Project Name 1
@@ -35,10 +51,10 @@ export default function Content() {
             </article>
             </a>
         </div>
-        <div class="my-1 w-full md:w-1/2 lg:my-4  lg:px-8 lg:w-1/3 h-40">
+        <div class="my-1 w-full md:w-1/2 lg:my-4  pb-4 lg:px-6 lg:w-1/3 h-56">
         <a class="no-underline  text-black" href="/prof/projectdetails">
             <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
-                <div class="px-6 py-4">
+                <div class="px-6 py-12">
                     <div class="font-bold text-xl mb-0 mt-6 flex justify-center items-center">
                         <h1 class="mt-2 mb-0 text-xl text-center hover:underline">
                             Project Name 1
@@ -51,10 +67,10 @@ export default function Content() {
             </article>
             </a>
         </div>
-        <div class="my-1 w-full md:w-1/2 lg:my-4  lg:px-8 lg:w-1/3 h-40">
+        <div class="my-1 w-full md:w-1/2 lg:my-4 pb-4 lg:px-6 lg:w-1/3 h-56">
         <a class="no-underline  text-black" href="/prof/projectdetails">
             <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
-                <div class="px-6 py-4">
+                <div class="px-6 py-12">
                     <div class="font-bold text-xl mb-0 mt-6 flex justify-center items-center">
                         <h1 class="mt-2 mb-0 text-xl text-center hover:underline">
                             Project Name 1
@@ -67,10 +83,10 @@ export default function Content() {
             </article>
             </a>
         </div>
-        <div class="my-1 w-full md:w-1/2 lg:my-4 lg:px-8 lg:w-1/3 h-40">
+        <div class="my-1 w-full md:w-1/2 lg:my-4 pb-4 lg:px-6 lg:w-1/3 h-56">
         <a class="no-underline  text-black" href="/prof/projectdetails">
             <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
-                <div class="px-6 py-4">
+                <div class="px-6 py-12">
                     <div class="font-bold text-xl mb-0 mt-6 flex justify-center items-center">
                         <h1 class="mt-2 mb-0 text-xl text-center hover:underline">
                             Project Name 1
@@ -83,10 +99,10 @@ export default function Content() {
             </article>
             </a>
         </div>
-        <div class="my-1 w-full md:w-1/2 lg:my-4 lg:px-8 lg:w-1/3 h-40">
+        <div class="my-1 w-full md:w-1/2 lg:my-4  pb-4 lg:px-6 lg:w-1/3 h-56">
         <a class="no-underline  text-black" href="/prof/projectdetails">
             <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
-                <div class="px-6 py-4">
+                <div class="px-6 py-12">
                     <div class="font-bold text-xl mb-0 mt-6 flex justify-center items-center">
                         <h1 class="mt-2 mb-0 text-xl text-center hover:underline">
                             Project Name 1
@@ -99,10 +115,10 @@ export default function Content() {
             </article>
             </a>
         </div>
-        <div class="my-1 w-full md:w-1/2 lg:my-4  lg:px-8 lg:w-1/3 h-40">
+        <div class="my-1 w-full md:w-1/2 lg:my-4 pb-4 lg:px-6 lg:w-1/3 h-56">
         <a class="no-underline  text-black" href="/prof/projectdetails">
             <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
-                <div class="px-6 py-4">
+                <div class="px-6 py-12">
                     <div class="font-bold text-xl mb-0 mt-6 flex justify-center items-center">
                         <h1 class="mt-2 mb-0 text-xl text-center hover:underline">
                             Project Name 1
@@ -115,26 +131,10 @@ export default function Content() {
             </article>
             </a>
         </div>
-        <div class="my-1 w-full md:w-1/2 lg:my-4 lg:px-8 lg:w-1/3 h-40">
+        <div class="my-1 w-full md:w-1/2 lg:my-4 pb-4 lg:px-6 lg:w-1/3 h-56">
         <a class="no-underline  text-black" href="/prof/projectdetails">
             <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-0 mt-6 flex justify-center items-center">
-                        <h1 class="mt-2 mb-0 text-xl text-center hover:underline">
-                            Project Name 1
-                        </h1>
-                    </div>
-                    <h6 class="text-xs text-center my-0">    
-                            Grade Level
-                    </h6>
-                </div>
-            </article>
-            </a>
-        </div>
-        <div class="my-1 w-full md:w-1/2 lg:my-4 lg:px-8 lg:w-1/3 h-40">
-        <a class="no-underline  text-black" href="/prof/projectdetails">
-            <article class="overflow-hidden border bg-white rounded-lg shadow-lg h-full">
-                <div class="px-6 py-4">
+                <div class="px-6 py-12">
                     <div class="font-bold text-xl mb-0 mt-6 flex justify-center items-center">
                         <h1 class="mt-2 mb-0 text-xl text-center hover:underline">
                             Project Name 1
