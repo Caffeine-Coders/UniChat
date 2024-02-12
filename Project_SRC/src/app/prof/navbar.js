9f598a7a9ff6f1fe540e59bb6fea0557d75d3072
Dheeraj <dheerajgoud28@gmail.com>
2024-02-11 23:30:40 -0500

    reroute

--- Project_SRC/src/app/prof/navbar.js
+++ Project_SRC/src/app/prof/navbar.js
@@ -1,11 +1,12 @@
-import bg from '../../../public/usericon.jpg'
-import {  Dropdown,  DropdownTrigger,  DropdownMenu,  DropdownSection, Button, DropdownItem} from "@nextui-org/react";
+import Link from "next/link"
 export default function Navdash() {
     return (
         <nav class="flex items-center justify-between flex-wrap p-6 bg-white">
-            <div class="flex items-center flex-shrink-0 mr-6">
-                <span class="font-bold text-2xl tracking-wide">UniChat</span>
-            </div>
+            <Link href="/">
+                <button class="flex items-center flex-shrink-0 mr-6">
+                    <span class="font-bold text-2xl tracking-wide">UniChat</span>
+                </button>
+            </Link>
             <button class="flex items-center gap-4">
                 <img class="w-6 h-6 rounded-full" src="https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg" alt=""/>
                 <div class="text-sm">
