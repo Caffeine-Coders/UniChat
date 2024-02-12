7840245b224f420aee3c87c35d0419cddc7ce675
Dheeraj <dheerajgoud28@gmail.com>
2024-02-11 21:27:56 -0500

    copy page

---
+++ Project_SRC/src/app/prof/copy/page.js
@@ -0,0 +1,10 @@
+import Navdash from "../navbar"
+import Content from "./content"
+export default function copier() {
+    return (
+        <div class= "h-screen bg-slate-100">
+        <Navdash/>
+        <Content/>
+        </div>
+    )
+  }
