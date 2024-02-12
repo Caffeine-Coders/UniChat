982bf3c5242b866f2b79c426d99a975ad303de32
Dheeraj <dheerajgoud28@gmail.com>
2024-02-12 15:42:01 -0500

    theme setting

--- Project_SRC/tailwind.config.js
+++ Project_SRC/tailwind.config.js
@@ -17,6 +17,57 @@ module.exports = {
     fontFamily: {
       mono: ["ui-monospace", "SFMono-Regular", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"],
     },
+    colors:{
+      'white': '#ffffff',
+      'black': '#000000',
+      'discordpurple': {
+        100: '#5964f2',
+        200: '#3744f0',
+      },
+      'discordgreen': {
+        100: '#46b866',
+        200:'#3ba95a',},
+      'dicordgreen1':{
+        100:'329457',
+        200:'258047',
+      },
+      'blue': {
+        100: '#4c8aed',
+        200: '#2563eb',
+      },  
+      'gray': {
+        100: '#f5f5f5',
+        200: '#eeeeee',
+        300: '#e0e0e0',
+        400: '#bdbdbd',
+        500: '#9e9e9e',
+        600: '#757575',
+        700: '#616161',
+        800: '#424242',
+        900: '#212121',
+      },
+      'slate': {
+        100: '#f8f9fa',
+        200: '#e9ecef',
+        300: '#dee2e6',
+        400: '#ced4da',
+        500: '#adb5bd',
+        600: '#868e96',
+        700: '#495057',
+        800: '#343a40',
+        900: '#212529',
+      },
+      'red': {700:'#b91c1c',},
+      'yellow': '#ffff00',
+      'green': '#00ff00',
+      'blue': '#0000ff',
+      'purple': '#800080',
+      'cyan': '#00ffff',
+      'magenta': '#ff00ff',
+      'transparent': 'transparent',
+      'current': 'currentColor',
+      
+    }
   },
   plugins: [],
 };
