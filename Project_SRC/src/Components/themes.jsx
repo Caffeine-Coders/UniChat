76bb1e805313b9e7a0dd028f703cde04bada9854
anudeep-17 <saivishnuanudeepk@gmail.com>
2024-02-11 23:52:32 -0500

    Theme setup

--- Project_SRC/src/Components/themes.jsx
+++ Project_SRC/src/Components/themes.jsx
@@ -1,4 +1,5 @@
-import { Roboto, Protest_Riot, Montserrat} from 'next/font/google'
+import { Roboto, Montserrat} from 'next/font/google'
+import { Protest_Riot } from 'next/font/google'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 
@@ -7,14 +8,7 @@ import { CssBaseline } from '@mui/material';
         subsets: ['latin'],
         display: 'swap',
     });
-
-    const protest_Riot = Protest_Riot({
-        weight: ['300', '400', '500', '700'],
-        subsets: ['latin'],
-        display: 'swap',
-    });
-
-    
+     
     const montserrat = Montserrat({
         weight: ['300', '400', '500', '700'],
         subsets: ['latin'],
@@ -22,57 +16,63 @@ import { CssBaseline } from '@mui/material';
     });
 
 
-    const theme = createTheme({
+    const lighttheme = createTheme({
         palette: 
         {
-            mode: 'dark',
-            primary:{},
-            secondary:{},
-            highlights:{},
-            whites:{},
-            blacks:{},
+            mode: 'light',
+            dark:
+            {
+                primary:{
+                    main: '1a1d1f',
+                    hover: '272b30',
+                    textcolor: '6e767e',
+                    ButtonColor: '5cDB985',
+                    ButtonHover: '050386B',
+                }
+            },
+            light:
+            {  
+                primary:
+                {
+                    main: '#fcfcfc',
+                    hover: '#efefef',
+                    textcolor: '#84888a',
+                    ButtonColor: '050386B',
+                    ButtonHover: '5cDB985',
+                },
+            },
         },
         typography: 
         {
             fontFamily: [
                 roboto.style.fontFamily,
-                protest_Riot.style.fontFamily,
+                // protest_Riot.style.fontFamily,
                 montserrat.style.fontFamily,
                 //add more if you feel any.
             ],
         },
-        components: 
+    });
+
+    const darktheme = createTheme({
+        palette: 
         {
-            MuiButton: {
-                styleOverrides: 
-                {
-                  root: 
-                  {
-                    fontSize: '1rem',
-                    borderRadius: '8px',
-                    padding: '10px 20px',
-                    fontWeight: 'bold',
-                  },
-                //   contained:
-                //   {
-                //     backgroundColor:'',
-                //     color: '',
-                //     '&:hover':{
-                //         backgroundColor:'',
-                //     }
-                //   },
-                //   outlined:
-                //   {
-                //     backgroundColor:'',
-                //     color: '',
-                //     '&:hover':{
-                //         borderColor:'',
-                //         backgroundColor:'',
-                //     }
-                //   },
-                },
-            },
+            mode: 'dark',
+            primary:{
+                main: '1a1d1f',
+                hover: '272b30',
+                textcolor: '6e767e',
+                ButtonColor: '5cDB985',
+                ButtonHover: '050386B',
+            }
+        },
+        typography: 
+        {
+            fontFamily: [
+                roboto.style.fontFamily,
+                montserrat.style.fontFamily,
+                //add more if you feel any.
+            ],
         },
     });
   
-  export default theme;
+  export {lighttheme, darktheme};
