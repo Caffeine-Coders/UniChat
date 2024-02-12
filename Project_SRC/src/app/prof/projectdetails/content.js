055ea035870c7c63cc767d82b8919c65b88fdc91
ForumShah8 <shahforu00@gmail.com>
2024-02-12 00:28:40 -0500

    project dashboard done

--- Project_SRC/src/app/prof/projectdetails/content.js
+++ Project_SRC/src/app/prof/projectdetails/content.js
@@ -1,12 +1,49 @@
 export default function Content(){
     return(
-        <div class="p-4 sm:ml-64">
+        <div class="p-4 sm:ml-64 -mt-8">
+            <span><a class="flex hover:underline" href="/prof"><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
+  </svg>Return to Dashboard</a></span>
             <div class="p-4 border-2 border-gray-200  rounded-lg dark:border-gray-700">
             <div class="flex items-center justify-center h-48 mb-4 rounded-lg bg-blue-200 ">
                 <h1 class="text-4xl font-extrabold text-gray-900 ">
                     Project Name 1
                 </h1>
             </div>
+            <div class="ml-28 mr-28  rounded-lg ">
+                <h1>
+                    Project Goal:
+                    <hr class="h-px  bg-gray-400 border-0"></hr>
+                </h1>
+            </div>
+            <div class="ml-28 mr-28 mb-8 mt-4 rounded-lg flex justify-between space-between">
+                <h5 class="flex ml-4 mr-4 text-justify">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </h5>
+            </div>
+
+            <div class="ml-28 mr-28  rounded-lg ">
+                <h1>
+                    Details
+                    <hr class="h-px  bg-gray-400 border-0"></hr>
+                </h1>
+            </div>
+            <div class="ml-28 mr-28  mt-4 rounded-lg flex justify-between space-between">
+                <h4 class="flex ml-4 mr-4">
+                Grade Level:
+                </h4>
+                <h4 class="mr-4">Grade Level Here</h4>
+                
+            </div>
+            <hr class="ml-28 mr-28  mt-4 h-px  bg-gray-200 border-0"></hr>
+            <div class="ml-28 mr-28 mb-8 mt-4 rounded-lg flex justify-between space-between">
+                <h4 class="flex ml-4 mr-4">
+                Subject Area(s):
+                </h4>
+                <h4 class="mr-4">Subject Areas Here</h4>
+                
+            </div>
+            
             <div class="ml-28 mr-28  rounded-lg ">
                 <h1>
                     Students
