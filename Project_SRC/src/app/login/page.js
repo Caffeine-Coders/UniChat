8c6dba6582424278429e7db9cd48bbc1bb468ae6
ForumShah8 <shahforu00@gmail.com>
2024-02-13 13:28:47 -0500

    login issue solved

---
+++ Project_SRC/src/app/login/page.js
@@ -0,0 +1,11 @@
+import Nav from "./navbar";
+import Content from "./content";
+export default function Login() {
+    return (
+      <div>
+        <Nav/>
+        <Content/>
+        {/* <a href="/prof">Dashboard Link</a> */}
+      </div>
+    );
+  }
