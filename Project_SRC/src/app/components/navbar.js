7ebd2506fea90a7f822c5848ef2d98aa9f5b2cd4
Dheeraj <dheerajgoud28@gmail.com>
2024-02-12 21:36:12 -0500

    all done

--- Project_SRC/src/app/components/navbar.js
+++ Project_SRC/src/app/components/navbar.js
@@ -9,7 +9,7 @@ export default function Nav() {
             </Link>
             <div class="flex items-center flex-shrink-0 mr-6">
                 <div>
-                    <a href="/login" class="inline-block px-4 py-2 leading-none border-2 border-transparent rounded hover:border hover:border-slate-500 hover:border-2">Login/Signup</a>
+                    <a href="/login" class="inline-block px-4 py-2 leading-none border-2 border-transparent rounded hover:text-discordpurple-400">Login/Signup</a>
                 </div>
             </div>
         </nav>
