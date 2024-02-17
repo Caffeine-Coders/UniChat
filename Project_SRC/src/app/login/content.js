20f337f5de630b80d4d5ef9b828cce3258186c86
Dheeraj <dheerajgoud28@gmail.com>
2024-02-17 17:16:30 -0500

    basic login

--- Project_SRC/src/app/login/content.js
+++ Project_SRC/src/app/login/content.js
@@ -3,24 +3,17 @@ import "../components/land.css"
 import {auth} from '../firebase'
 import { signInWithPopup, GoogleAuthProvider} from "firebase/auth"
 const provider = new GoogleAuthProvider();
-
+import { Router } from "next/navigation";
 const handler = () => {
     signInWithPopup(auth, provider)
     .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
-      // The signed-in user info.
       const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
     }).catch((error) => {
-      // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
       const email = error.customData.email;
-      // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
     });
@@ -35,12 +28,20 @@ export default function content() {
                 Sign In
             </div>
         </div>
+        <div>
+        <div>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                  </div>
+                  <div>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class=" mt-5 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
+        </div>
         <div class="p-6 pt-0 mt-5">
-            <a class="inline-flex align-middle items-center w-10/12 justify-center px-5 py-3 text-base text-xl text-center text-black bg-slate-200 rounded-lg hover:bg-discordpurple-300  focus:ring-4 focus:ring-blue-100" href="/prof">    
-                Sign in with 
-                <img class="w-6 h-6 ml-3" src="https://img.icons8.com/color/48/000000/google-logo.png"/>
+            <button class="inline-flex align-middle items-center w-10/12 justify-center px-5 py-3 text-base text-xl text-center text-black bg-slate-200 rounded-lg hover:bg-discordpurple-300  focus:ring-4 focus:ring-blue-100">    
+                Sign in
+                {/* <img class="w-6 h-6 ml-3" src="https://img.icons8.com/color/48/000000/google-logo.png"/> */}
                
-            </a>
+            </button>
         </div>
         <div class="text-sm -mt-5">
             Don't have an account? 
