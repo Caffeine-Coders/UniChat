606ddd00bb17edfba7c911bd5873952ca1b158a4
Satwik Bhasin <satwikbhasin@gmail.com>
2024-02-23 23:01:16 -0500

    final_commit

--- Project_SRC/src/Services/MongoDB_Routines.js
+++ Project_SRC/src/Services/MongoDB_Routines.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require("mongodb");
-require("dotenv").config();
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
--- Project_SRC/src/app/page.js
+++ Project_SRC/src/app/page.js
@@ -1,6 +1,5 @@
 import LandingPage from "../Components/Landingpage/Landing";
 
-
 export default function Home() {
   return <LandingPage />;
 }
