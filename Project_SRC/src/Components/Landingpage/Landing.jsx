6d9c2eff9efa1fe0e66826ff77bcaf6a2df26a7d
anudeep-17 <anudeepsai88@gmail.com>
2024-02-24 19:45:46 -0500

    landing page edits

--- Project_SRC/src/Components/Landingpage/Landing.jsx
+++ Project_SRC/src/Components/Landingpage/Landing.jsx
@@ -24,14 +24,14 @@ export default function LandingPage() {
                 <LoginIcon  sx={{ mr: 1, color: "white" }} />
                 Login
             </Fab>
-            <ThemeProvider theme={lighttheme}>
+            <ThemeProvider theme={darktheme}>
             <Box
                 sx={{
                     display: "flex",
                     width: "100%",
                     height: "100%",
                     zIndex: 9999,
-                    backgroundImage: "linear-gradient(145deg, hsl(0deg 0% 87%) 0%, hsl(270deg 8% 80%) 20%, hsl(270deg 16% 74%) 44%, hsl(270deg 24% 67%) 65%, hsl(270deg 32% 61%) 76%, hsl(269deg 40% 54%) 82%, hsl(270deg 48% 48%) 88%, hsl(270deg 56% 41%) 92%, hsl(270deg 64% 35%) 96%, hsl(270deg 72% 28%) 100%)",
+                    backgroundImage: "linear-gradient(145deg, hsl(0deg 0% 0%) 0%, hsl(270deg 75% 3%) 40%, hsl(271deg 74% 6%) 62%, hsl(270deg 71% 9%) 72%, hsl(270deg 72% 13%) 79%, hsl(269deg 72% 15%) 84%, hsl(270deg 73% 19%) 89%, hsl(269deg 71% 22%) 93%, hsl(270deg 72% 25%) 96%, hsl(270deg 72% 28%) 100%)",
                     flexDirection: "column",
                 }}
             >
@@ -51,7 +51,8 @@ export default function LandingPage() {
                             fontFamily: '"Kode Mono", monospace',
                             fontWeight: 100,
                             textAlign : "center",
-                            color: (theme) => theme.palette.primary.rare,
+                            color: darktheme.palette.primary.textcolor,
+                            "&:hover": {color: darktheme.palette.primary.ButtonColor, cursor: "pointer"},
                             mb: 2,
                             mt: 2,
                             fontSize: 100,
@@ -67,77 +68,78 @@ export default function LandingPage() {
                 <motion.div
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1, y: 10 }}
-                    transition={{ delay: 1 }}
+                    transition={{ delay: 0.5 }}
                     exit={{ opacity: 1 }} // Set exit property to keep the final state
                 >
-                <Box sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    height: "100%",
-                    mt: 10,
-                    ml: 5
-                }}>
-                <Box position={'relative'}  >
-                    <Box
-                        position={"absolute"}
-                            sx={{
-                            mr: 2,
-                            ml: 2,
-                            borderRadius: 8, // Add border radius for a modular design
-                            overflow: "hidden", // Hide overflow to maintain border radius
-                            boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.1)' // Add shadow for depth effect
-                            }}
-                        >
-                            <motion.div
-                                initial={{ opacity: 0 }}
-                                whileInView={{ opacity: 1 }}
-                            >
-                                <Image src={logo.src} alt="Image" height={450} width={450} />
-                            </motion.div>
-                        </Box>
-            
+                    <Box sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        height: "100%",
+                        mt: 10,
+                        ml: 5
+                    }}>
+                    <Box position={'relative'}  sx={{
+                        '&:hover':{
+                            transform: "rotate(2deg)",
+                            transition: "transform 0.5s"
+                        }
+                    }} >
                         <Box
-                        position={"relative"}
+                            position={"absolute"}
+                                sx={{
+                                mr: 2,
+                                ml: 2,
+                                borderRadius: 8, // Add border radius for a modular design
+                                overflow: "hidden", // Hide overflow to maintain border radius
+                                boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.1)', // Add shadow for depth effect
+                                }}
+                            >
+                                <Image src={logo.src} alt="Image" height={450} width={450}/>
+                            </Box>
+                
+                            <Box
+                            position={"relative"}
+                                sx={{
+                                mr: 2,
+                                ml: 10,
+                                borderRadius: 8, // Add border radius for a modular design
+                                overflow: "hidden", // Hide overflow to maintain border radius
+                                zIndex: 1, // Ensure it's above the first box
+                                transform: "rotate(10deg)", // Tilt the box slightly
+                                boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.1)' // Add shadow for depth effect
+                                }}
+                            >
+                                <Image src={layerer.src} alt="Image" height={450} width={450} />
+                            </Box>
+                        </Box>
+
+                        <Box
+                            position="relative"
                             sx={{
-                            mr: 2,
-                            ml: 10,
-                            borderRadius: 8, // Add border radius for a modular design
-                            overflow: "hidden", // Hide overflow to maintain border radius
-                            zIndex: 1, // Ensure it's above the first box
-                            transform: "rotate(10deg)", // Tilt the box slightly
-                            boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.1)' // Add shadow for depth effect
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                flexDirection: "column",
+                                width: "50%",
+                                height: "100%",
+                                ml: 10
                             }}
                         >
-                            <Image src={layerer.src} alt="Image" height={450} width={450} />
+                                <Typography variant = "body1" sx={{ 
+                                    fontFamily: '"Kode Mono", monospace',
+                                    fontWeight: 100,
+                                    textAlign : "center",
+                                    color: darktheme.palette.primary.textcolor,
+                                    "&:hover": {color: darktheme.palette.primary.ButtonColor, cursor: "pointer"},
+                                    mb: 2,
+                                    mt: 2,
+                                    fontSize: 30,
+                                    display: "inline-block",
+                                }}> 
+                                    Unichat is the ultimate project management application that seamlessly integrates with Google Docs and leverages the power of Discord for communication.  
+                                </Typography>
                         </Box>
                     </Box>
-
-                    <Box
-                        position="relative"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            flexDirection: "column",
-                            width: "50%",
-                            height: "100%",
-                            ml: 10
-                        }}
-                    >
-                            <Typography variant = "body1" sx={{ 
-                                fontFamily: '"Kode Mono", monospace',
-                                fontWeight: 100,
-                                textAlign : "center",
-                                color: (theme) => theme.palette.primary.hover,
-                                mb: 2,
-                                mt: 2,
-                                fontSize: 30,
-                                display: "inline-block",
-                            }}> 
-                                Unichat is the ultimate project management application that seamlessly integrates with Google Docs and leverages the power of Discord for communication.  
-                            </Typography>
-                    </Box>
-                </Box>
                 </motion.div>
 
                 {/* second about page */}
@@ -172,7 +174,8 @@ export default function LandingPage() {
                                    fontFamily: '"Kode Mono", monospace',
                                     fontWeight: 100,
                                     textAlign : "center",
-                                    color: (theme) => theme.palette.primary.hover,
+                                    color: darktheme.palette.primary.textcolor,
+                                    "&:hover": {color: darktheme.palette.primary.ButtonColor, cursor: "pointer"},
                                     mb: 2,
                                     mt: 2,
                                     fontSize: 30,
@@ -181,7 +184,13 @@ export default function LandingPage() {
                                     With Unichat, teams can collaborate effectively, manage projects efficiently, and brainstorm ideas effortlessly with the help of AI.
                                 </Typography>
                         </Box>
-                        <Box position={'relative'} >
+                        <Box position={'relative'} sx={{
+                            '&:hover':{
+                                transform: "rotate(2deg)",
+                                transition: "transform 0.5s"
+                            }
+                        
+                        }}>
                             <Box
                                 position={"absolute"}
                                     sx={{
@@ -194,7 +203,7 @@ export default function LandingPage() {
                                 >
                                     <Image src={layerer.src} alt="Image" height={450} width={450} />
                                 </Box>
-                                {/* Second Box */}
+                          
                                 <Box
                                 position={"relative"}
                                     sx={{
@@ -229,7 +238,13 @@ export default function LandingPage() {
                         mt: 15,
                         ml: 5
                     }}>
-                    <Box position={'relative'}  >
+                    <Box position={'relative'}   sx={{
+                        '&:hover':{
+                            transform: "rotate(2deg)",
+                            transition: "transform 0.5s"
+                        }
+                    
+                    }}>
                         <Box
                             position={"absolute"}
                                 sx={{
@@ -275,7 +290,8 @@ export default function LandingPage() {
                                     fontFamily: '"Kode Mono", monospace',
                                     fontWeight: 100,
                                     textAlign : "center",
-                                    color: (theme) => theme.palette.primary.hover,
+                                    color: darktheme.palette.primary.textcolor,
+                                    "&:hover": {color: darktheme.palette.primary.ButtonColor, cursor: "pointer"},
                                     mb: 2,
                                     mt: 2,
                                     fontSize: 30,
@@ -322,7 +338,8 @@ export default function LandingPage() {
                                     fontFamily: '"Kode Mono", monospace',
                                     fontWeight: 100,
                                     textAlign : "center",
-                                    color: (theme) => theme.palette.primary.hover,
+                                    color: darktheme.palette.primary.textcolor,
+                                    "&:hover": {color: darktheme.palette.primary.ButtonColor, cursor: "pointer"},
                                     mb: 2,
                                     mt: 2,
                                     fontSize: 30,
@@ -331,7 +348,14 @@ export default function LandingPage() {
                                     With AI assistance, students can harness their creativity and brainstorm innovative ideas, fostering a collaborative learning environment and enhancing problem-solving skills.
                                 </Typography>
                         </Box>
-                        <Box position={'relative'} >
+                        <Box position={'relative'} 
+                            sx={{
+                                '&:hover':{
+                                    transform: "rotate(2deg)",
+                                    transition: "transform 0.5s"
+                                }
+                            }}
+                        >
                             <Box
                                 position={"absolute"}
                                     sx={{
--- Project_SRC/src/app/page.js
+++ Project_SRC/src/app/page.js
@@ -3,5 +3,7 @@ import LandingPage from "../Components/Landingpage/Landing";
 
 export default function Home() {
  
-  return <LandingPage />;
+  return <div style={{scrollBehavior: "smooth"}}>
+         <LandingPage />
+          </div>
 }
