c4e2806bfad9dfbcc452909971c484fa1bc2e3de
Satwik Bhasin <satwikbhasin@gmail.com>
2024-02-27 14:27:44 -0500

    glow_fix

--- Project_SRC/src/Components/HomeFrame/HomeFrame.jsx
+++ Project_SRC/src/Components/HomeFrame/HomeFrame.jsx
@@ -117,7 +117,7 @@ export default function HomeComponent() {
                   justifyContent: "center",
                   display: "flex",
                   backgroundColor: discordServerId === "noProjectSelected" ? theme.palette.primary.main : "#2e3035",
-                  boxShadow: "0 10px 100px 0 rgba(31, 38, 135, 0.7)",
+                  boxShadow: "0 0px 25px 0 rgba(31, 38, 135, 0.7)",
                   backdropFilter: "blur(4px)",
                   WebkitBackdropFilter: "blur(4px)",
                   border: theme.palette.primary.main,
