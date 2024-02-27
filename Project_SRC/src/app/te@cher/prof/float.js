e756babdeb00428fb8bc0324c982ed3b6ee14cef
ForumShah8 <shahforu00@gmail.com>
2024-02-26 22:53:01 -0500

    json content print

--- Project_SRC/src/app/te@cher/prof/float.js
+++ Project_SRC/src/app/te@cher/prof/float.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-import { Typography } from '@mui/material';
+import { Typography, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -12,6 +12,7 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { styled } from '@mui/material';
+
 import Papa from 'papaparse'
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -25,9 +26,25 @@ const VisuallyHiddenInput = styled('input')({
     width: 1,
   });
 
+  const getEmailAndName = () => {
+    if (parsedData) {
+      return parsedData.map(row => {
+        if (row['Email Address']) {
+          return {
+            email: row['Email Address'],
+            name: row['First Name'] + ' ' + row['Last Name'],
+          };
+        }
+        return null;
+      }).filter(entry => entry !== null);
+    }
+    return [];
+  };
+
 export default function FloatingActionButtons() {
     const [hovering, setHovering] = React.useState(false);
     const [open, setOpen] = React.useState(false);
+    const [open2, setOpen2] = React.useState(false)
     const [filename,setfilename] = React.useState(null)
     const [parsedData, setParsedData] = React.useState(null);
     const [className, setClassName] = React.useState('');
@@ -82,7 +99,13 @@ export default function FloatingActionButtons() {
         }
         return [];
     };
-    
+    const handleTemp = () =>{
+      setOpen(false);
+      setOpen2(true);
+    }
+    const handleClose2 = () =>{
+      setOpen2(false)
+    }
     const handleSubmit = (event) => {
         event.preventDefault();
         const emailAndName = getEmailAndName();
@@ -95,9 +118,11 @@ export default function FloatingActionButtons() {
         } else {
           console.log("class name",className);
           console.log("email name",emailAndName);
-          handleClose();
+          handleTemp();
         }
     };
+
+    
     return (
         <>
         <Box sx={{ position: 'fixed', bottom: '20px', right: '20px' }}>
@@ -149,7 +174,10 @@ export default function FloatingActionButtons() {
       variant="contained"
       tabIndex={-1}
       startIcon={<CloudUploadIcon />}
-      sx={{justifyContent:'justify', alignItems:'center'}}
+      sx={{justifyContent:'justify', alignItems:'center', backgroundColor:'#5964f2','&:hover': {
+        opacity:'80%',
+        backgroundColor:'#5964f2' 
+    },}}
     >
       Upload file
       <VisuallyHiddenInput type="file" onChange={handleFileName} required />
@@ -161,9 +189,43 @@ export default function FloatingActionButtons() {
     </div>
            </DialogContent>
            <DialogActions>
-             <Button onClick={handleClose}>Cancel</Button>
-             <Button type='submit' onClick={(event) => handleSubmit(event)}>Create Class</Button>
+             <Button onClick={handleClose} sx={{color:'#5964f2', backgroundColor:hovering?'#d5d8fb':'transparent'}}>Cancel</Button>
+             <Button type='submit' sx={{color:'#5964f2'}} onClick={(event) => handleSubmit(event)}>Create Class</Button>
            </DialogActions>
+         </Dialog>
+         <Dialog
+           open={open2}
+           onClose={handleClose2}
+         >
+           <DialogTitle>Classroom Created</DialogTitle>
+           <hr />
+           <DialogContent>
+             <DialogContentText sx={{textAlign:'center', color:'black'}}>
+               Class Name: {className}
+             </DialogContentText>
+             <div class=" mt-4">
+
+    <Table>
+            <TableHead >
+              <TableRow >
+                <TableCell sx={{ color:'black', fontWeight:'bold'}}>Email</TableCell>
+                <TableCell sx={{ color:'black', fontWeight:'bold'}}>Name</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {getEmailAndName().map((entry, index) => (
+                <TableRow key={index}>
+                  <TableCell>{entry.email}</TableCell>
+                  <TableCell>{entry.name}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+    </div>
+           </DialogContent>
+           <DialogActions>
+             <Button onClick={handleClose2} sx={{color:'#5964f2', backgroundColor:hovering?'#d5d8fb':'transparent'}}>Close</Button>
+                 </DialogActions>
          </Dialog>
          </>
     );
