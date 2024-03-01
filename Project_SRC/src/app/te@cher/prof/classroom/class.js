2304b3521c15b57bb095941a1133d5eba8134868
Dheeraj <dheerajgoud28@gmail.com>
2024-02-29 23:09:23 -0500

    classroom page done

--- Project_SRC/src/app/te@cher/prof/classroom/class.js
+++ Project_SRC/src/app/te@cher/prof/classroom/class.js
@@ -1,163 +1,19 @@
 "use client";
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { styled, useTheme } from '@mui/material/styles';
 import Navdash from "../navbar.js"
 import { CssBaseline } from '@mui/material';
-import MuiDrawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import GridViewIcon from '@mui/icons-material/GridView';
-import SchoolIcon from '@mui/icons-material/School';
 import Content from './content.js';
-
-const drawerWidth = 240;
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  padding:'10px',
-  overflowX: 'hidden',
-  background:'transparent',
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  padding:'10px',
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-    
-  },
-  background:'transparent',
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-  background:'transparent',
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    background:'transparent',
-    boxSizing: 'border-box',
-    
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-      
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
 export default function Class() {
-    const [open, setOpen] = React.useState(false);
-  const [selectedIndex, setSelectedIndex] = React.useState(1); 
 
-  const[addClassroom,setaddClassroom] = React.useState(false)
-  const[viewClassroom, setviewclassroom] = React.useState(true)
-  const handleListItemClick = (index) => { 
-    setSelectedIndex(index);
-    console.log("clicked",index)
-    const indexval = JSON.stringify(index);
-    localStorage.setItem("indexval", indexval, () => {
-    });
-    if (index === 0){
-      setaddClassroom(true)
-      setviewclassroom(false)
-    }
-    else if(index === 1){
-      setviewclassroom(true)
-      setaddClassroom(false)
-    }
-  };
-  React.useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const indexfromlocal = JSON.parse(localStorage.getItem("indexval"));
-      if (indexfromlocal!==null){
-        setSelectedIndex(indexfromlocal)
-        handleListItemClick(indexfromlocal)
-      }
-    }
-  });
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-  const sidebar=['New Classroom', 'All Classrooms']
 
     return (
         <Box sx={{ display: 'flex' }}>
-  <CssBaseline />
-  <Navdash/>
-  <Drawer variant="permanent" open={open} onMouseEnter={handleDrawerOpen} onMouseLeave={handleDrawerClose} >
-    <DrawerHeader>
-     {/* <MenuIcon sx={{color:'grey'}}/> */}
-    </DrawerHeader>
-    <Divider />
-    <List>      {sidebar.map((text, index) => (
-        <ListItem key={text} disablePadding sx={{ display: 'block',  backgroundColor: selectedIndex === index? '#d5d8fb': 'transparent' }}>
-          
-          <ListItemButton
-            sx={{
-              minHeight: 48,
-              justifyContent: open ? 'initial' : 'center',
-              px: 2.5,
-              
-            }}
- 
-            onClick={() => handleListItemClick(index)} 
-            selected={selectedIndex === index}
-          >
-            <ListItemIcon
-              sx={{
-                minWidth: 0,
-                mr: open ? 3 : 'auto',
-                justifyContent: 'center',
+          <CssBaseline />
+          <Navdash/>
+          <Content/>
+        </Box>
 
-              }}
-
-            >
-              {index === 0 ? <AddBoxIcon />: ''}
-              {index === 1 ? <GridViewIcon/>:''}
-              {index === 2 ? <SchoolIcon/>:''}
- 
-            </ListItemIcon>
-            <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
-    <Divider />
-  </Drawer>
-  <Content/>
-</Box>
-
-)
+  )
 }
         
--- Project_SRC/src/app/te@cher/prof/classroom/content.js
+++ Project_SRC/src/app/te@cher/prof/classroom/content.js
@@ -1,44 +1,346 @@
 "use client";
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-
+import Add from '@mui/icons-material/Add';
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+import Groups2OutlinedIcon from '@mui/icons-material/Groups2Outlined';
+import ticon from "../../../../../public/ticon.png"
+import sicon from "../../../../../public/sicon.png"
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import "../../components/dash.css"
+import FilterListIcon from '@mui/icons-material/FilterList';
 export default function Content() {
+    const [classname, setClassname] = useState('');
+    const [classnumber, setClassnumber] = useState('');
+    const [hover, setHover] = useState(false);
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        setClassname(JSON.parse(localStorage.getItem("classname")));
+        setClassnumber(JSON.parse(localStorage.getItem("classnumber")));
+      }
+    }, []);
 
+        const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      }));
+      
+      const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }));
+      const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        width: '100%',
+        '& .MuiInputBase-input': {
+          padding: theme.spacing(1, 1, 1, 0),
+          // vertical padding + font size from searchIcon
+          paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+          transition: theme.transitions.create('width'),
+          [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+              width: '20ch',
+            },
+          },
+        },
+      }));
   return (
     <>
-    <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop:'40px' }}>
-    <Box sx={{ display: 'flex' }}>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                        {/* <Paper sx={{ padding: 2, color: 'text.secondary', backgroundColor:'transparent', borderRadius: '8px' }}> */}
-                            <div class="h-80 flex items-center justify-center bg-gray-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20">
-                            <Typography variant="h2" component="h2" sx={{fontFamily: 'caveat'}}>
-                                680 Master's Project
-                            </Typography>
-                            </div>
-                        {/* </Paper> */}
-                </Grid>
-                <Grid item xs={7}>
-                    <div class="h-40 flex items-center justify-center bg-gray-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20">
-                        <Typography variant="h2" component="h2" sx={{fontFamily: 'caveat'}}>
-                            Projects
-                        </Typography>
-                    </div>
-                </Grid>
-                <Grid item xs={5}>
-                    <div class="h-40 flex items-center justify-center bg-gray-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20">
-                        <Typography variant="h2" component="h2" sx={{fontFamily: 'caveat'}}>
-                            Students
-                        </Typography>
-                    </div>
-                </Grid>
+    <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop:'40px', display: 'flex', marginLeft: '10%', marginRight: '10%' }}>
+        <Grid container spacing={4}>
+            <Grid item xs={12}>
+                <div class="flex rounded-md mt-8">
+                <Typography variant="h2" component="h2" sx={{fontFamily: 'caveat'}}>
+                    Welcome to {classnumber+ " " +classname}
+                </Typography>
+                </div>
             </Grid>
-        </Box>
-        </Box>
+            <Grid item xs={12}>
+                <div class="flex rounded-md mt-8">
+                <Typography variant="h5" component="h2" fontWeight="bold" letterSpacing="2px">
+                    Overview
+                </Typography>
+                </div>
+            </Grid>
+            <Grid item xs={3}>
+            <button class="focus:outline-none w-full hover:bg-gray-300 hover:rounded-xl ">
+                <div class="bg-white rounded-xl h-40 shadow-lg">
+                    <div style={{marginLeft:'20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', height: '100%'}}>
+                        <AddBoxOutlinedIcon sx={{ fontSize: 30, marginBottom: '15px' }} />
+                        <Typography variant="h5" component="h2">
+                            Create Project
+                        </Typography>
+                    </div>
+                </div>
+            </button>
+            </Grid>
+            <Grid item xs={3}>
+            <button class="focus:outline-none w-full hover:bg-gray-300 hover:rounded-xl ">
+                <div class="bg-white rounded-xl h-40 shadow-lg">
+                    <div style={{marginLeft:'20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', height: '100%'}}>
+                        <img src={sicon.src} alt="sicon" style={{width: '30px', marginBottom: '15px'}} />
+                        <Typography variant="h5" component="h2">
+                            Add Students
+                        </Typography>
+                    </div>
+                </div>
+            </button>
+            </Grid>
+            <Grid item xs={3}>
+            <button class="focus:outline-none w-full hover:bg-gray-300 hover:rounded-xl ">
+                <div class="bg-white rounded-xl h-40 shadow-lg">
+                    <div style={{marginLeft:'20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', height: '100%'}}>
+                        <img src={ticon.src} alt="ticon" style={{width: '30px', marginBottom: '15px'}} />
+                        <Typography variant="h5" component="h2">
+                            Add Co Instructors
+                        </Typography>
+                    </div>
+                </div>
+            </button>
+            </Grid>
+            <Grid item xs={3}>
+            <button class="focus:outline-none w-full hover:bg-gray-300 hover:rounded-xl ">
+                <div class="bg-white rounded-xl h-40 shadow-lg">
+                    <div style={{marginLeft:'20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', height: '100%'}}>
+                        <Groups2OutlinedIcon sx={{ fontSize: 30, marginBottom: '15px' }} />
+                        <Typography variant="h5" component="h2">
+                            View Members
+                        </Typography>
+                        <Typography variant="h6" component="h2" color="gray">
+                            3 members
+                        </Typography>
+                    </div>
+                </div>
+            </button>
+            </Grid>
+            <Grid item xs={12}>
+                <div class="flex rounded-md mt-8">
+                <Typography variant="h5" component="h2" fontWeight="bold" letterSpacing="2px">
+                    Projects
+                </Typography>
+                </div>
+            </Grid> 
+            <Grid item xs={12}>
+                <div class="flex rounded-md w-full">
+                <Search style={{ display: 'flex', width: '100%', background: 'linear-gradient(to right, white, transparent)', height: '50px', borderRadius: '15px'}}>
+                    <SearchIconWrapper>
+                    <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                    placeholder="Search Projects..."
+                    inputProps={{ 'aria-label': 'search' }}
+                    style={{background:'transparent', height: '100%', width: 'full', padding: '0', margin: '0', flex: '1' }}
+                    />
+                </Search>
+                </div>
+            </Grid> 
+            {/* <Grid item xs={2}>
+                <div class="flex rounded-md w-full">
+                <Button 
+                    onMouseHover={() => setHover(true)}
+                    onMouseOut={() => setHover(false)}
+                    style={{backgroundColor: '#e3d0f5', color: hover ? 'blue':'black', borderRadius: '15px', height: '50px', width: '100%', background: 'linear-gradient(to right, transparent, transparent, gray'}} startIcon={<FilterListIcon />}>
+                    
+                </Button>
+                </div>
+            </Grid>  */}
+            <Grid item xs={3}>
+                <Card sx={{ maxWidth: 345, borderRadius: '15px' }}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image="https://img.freepik.com/free-photo/technology-human-touch-background-modern-remake-creation-adam_53876-129794.jpg?t=st=1709264522~exp=1709268122~hmac=37495c2f747709fb91df549997f05b4fa7716d2b9c2bd9cebeab5eaa4ebc67b6&w=2000"
+                        style={{ height: '200px' }}
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Project 1
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Description
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">View</Button>
+                    </CardActions>
+                </Card>
+            </Grid>  
+            <Grid item xs={3}>
+                <Card sx={{ maxWidth: 345, borderRadius: '15px' }}>
+                    <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://img.freepik.com/free-vector/business-team-discussing-ideas-startup_74855-4380.jpg?t=st=1709264619~exp=1709268219~hmac=02dbc5612dd7fda27d7fa496c363e0210ffd860b4525359a2d7fb922c80b6415&w=2000"
+                    style={{ height: '200px' }}
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Project 2
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Description
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">View</Button>
+                    </CardActions>
+                </Card>
+            </Grid>  
+            <Grid item xs={3}>
+                <Card sx={{ maxWidth: 345, borderRadius: '15px' }}>
+                    <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://img.freepik.com/free-vector/organizing-projects-concept-illustration_114360-542.jpg?t=st=1709264652~exp=1709268252~hmac=e920e67e63c1dad31bc765d6e11bb104962195a5e15e9a1b710ff32fbea9a7e9&w=1380"
+                    alt="green iguana"
+                    style={{ height: '200px' }}
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Project 3
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Description
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">View</Button>
+                    </CardActions>
+                </Card>
+            </Grid>  
+            <Grid item xs={3}>
+                <Card sx={{ maxWidth: 345, borderRadius: '15px' }}>
+                    <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://img.freepik.com/free-vector/project-management-business-process-planning-workflow-organization-colleagues-working-together-teamwork_335657-2469.jpg?t=st=1709264673~exp=1709268273~hmac=e2c3f6cc36f72bddbe55c2584d0f21348c438e981c9c30f37667154bba83bf5e&w=2000"
+                    style={{ height: '200px' }}
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Project 4
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Description
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">View</Button>
+                    </CardActions>
+                </Card>
+            </Grid>  
+            <Grid item xs={3}>
+                <Card sx={{ maxWidth: 345, borderRadius: '15px' }}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image="https://img.freepik.com/free-photo/technology-human-touch-background-modern-remake-creation-adam_53876-129794.jpg?t=st=1709264522~exp=1709268122~hmac=37495c2f747709fb91df549997f05b4fa7716d2b9c2bd9cebeab5eaa4ebc67b6&w=2000"
+                        style={{ height: '200px' }}
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Project 5
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Description
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">View</Button>
+                    </CardActions>
+                </Card>
+            </Grid>  
+            <Grid item xs={3}>
+                <Card sx={{ maxWidth: 345, borderRadius: '15px' }}>
+                    <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://img.freepik.com/free-vector/business-team-discussing-ideas-startup_74855-4380.jpg?t=st=1709264619~exp=1709268219~hmac=02dbc5612dd7fda27d7fa496c363e0210ffd860b4525359a2d7fb922c80b6415&w=2000"
+                    style={{ height: '200px' }}
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Project 6
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Description
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">View</Button>
+                    </CardActions>
+                </Card>
+            </Grid>  
+            <Grid item xs={3}>
+                <Card sx={{ maxWidth: 345, borderRadius: '15px' }}>
+                    <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://img.freepik.com/free-vector/organizing-projects-concept-illustration_114360-542.jpg?t=st=1709264652~exp=1709268252~hmac=e920e67e63c1dad31bc765d6e11bb104962195a5e15e9a1b710ff32fbea9a7e9&w=1380"
+                    alt="green iguana"
+                    style={{ height: '200px' }}
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Project 7
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Description
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">View</Button>
+                    </CardActions>
+                </Card>
+            </Grid>  
+            <Grid item xs={3}>
+                <Card sx={{ maxWidth: 345, borderRadius: '15px' }}>
+                    <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://img.freepik.com/free-vector/project-management-business-process-planning-workflow-organization-colleagues-working-together-teamwork_335657-2469.jpg?t=st=1709264673~exp=1709268273~hmac=e2c3f6cc36f72bddbe55c2584d0f21348c438e981c9c30f37667154bba83bf5e&w=2000"
+                    style={{ height: '200px' }}
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Project 8
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Description
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">View</Button>
+                    </CardActions>
+                </Card>
+            </Grid>  
+        </Grid>
     </Box>
     </>
   );
