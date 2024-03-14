4279a35fa5a1118453123cc620a1a17a67217616
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-03-14 14:09:57 -0400

    native chat work done

--- Project_SRC/src/Components/HomeFrame/HomeFrame.jsx
+++ Project_SRC/src/Components/HomeFrame/HomeFrame.jsx
@@ -187,7 +187,7 @@ export default function HomeComponent() {
                   {chatPlatform === "discord" ? (
                     <Discord props={[discordServerId, projects]} />
                   ) : (
-                    <Nativechat />
+                    <Nativechat project={projects}/>
                   )}
                 </ThemeContext.Provider>
               </Box>
