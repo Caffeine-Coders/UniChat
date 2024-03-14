19f6f24f544c062904ce306fd633310736b4d13f
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-03-14 03:10:11 -0400

    the backend work is done

--- Project_SRC/src/app/api/students/projects/getStudentProjects/route.js
+++ Project_SRC/src/app/api/students/projects/getStudentProjects/route.js
@@ -3,16 +3,17 @@ import { client } from "../../../../../Services/MongoDB_Routines";
 
 export async function GET(request) {
   const url = new URL(request.url);
+  const databasename = url.searchParams.get("databasename");
   const studentId = new URLSearchParams(url.search).get("studentId");
 
   // Connect to the MongoDB database
-  const db = client.db("universityatalbanyDB");
+  const db = client.db(databasename);
 
   // Get the collection you want to work with
   const collection = db.collection("projects");
 
-  // Query the collection for a document with the given studentId
-  const projects = await collection.find({ studentId: studentId }).toArray();
+  // Query the collection for documents with the given studentId in the studentIds array
+  const projects = await collection.find({ studentIds: { $in: [studentId] } }).toArray();
 
   // Check if any projects were found
   if (projects.length > 0) {
