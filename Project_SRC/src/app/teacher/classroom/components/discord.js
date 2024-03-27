d8f4507af008bf9cfd7e01b61fde18fc31d5f3c4
ForumShah8 <shahforu00@gmail.com>
2024-03-26 22:11:18 -0400

    discord change

--- Project_SRC/src/app/teacher/classroom/components/discord.js
+++ Project_SRC/src/app/teacher/classroom/components/discord.js
@@ -34,7 +34,7 @@ export default function Discord() {
        <div style={{ height: '85vh', width: '100%', borderRadius: '10px', overflow: 'hidden', position: 'relative' }}>
         
         <iframe
-          src={`https://e.widgetbot.io/channels/1204543420148752434/1204543420148752437`}
+          src={`https://e.widgetbot.io/channels/1217972638920609922/1217972639553945771`}
           style={{ border: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
         />
       </div> 
