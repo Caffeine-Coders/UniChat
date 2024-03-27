cb8308368a059e07978beca084bd7421c27e43a0
ForumShah8 <shahforu00@gmail.com>
2024-03-26 23:49:05 -0400

    getting classes

--- Project_SRC/src/app/teacher/api/getclass/route.js
+++ Project_SRC/src/app/teacher/api/getclass/route.js
@@ -2,9 +2,13 @@ import { NextResponse } from "next/server";
 import { client, connectToMongoDB } from "../../dbconnections/mongo.js"
 
 export async function GET(request) {
+    let currentemail = ""
     console.log("route here")
-    const currentemail = localStorage.getItem("Temail")
+    if (typeof window !== undefined){
+    currentemail = localStorage.getItem("Temail")
     console.log("got email",currentemail)
+    }
+
   // Connect to the MongoDB database
     const db = client.db("universityatalbanyDB");
   // Get the collection you want to work with
