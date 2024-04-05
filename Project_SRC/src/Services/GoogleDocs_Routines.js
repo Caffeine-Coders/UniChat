751555dcb9e721df728075e741e47ccebff948d0
Satwik Bhasin <satwikbhasin@gmail.com>
2024-04-05 14:48:20 -0400

    latest

--- Project_SRC/src/Services/GoogleDocs_Routines.js
+++ Project_SRC/src/Services/GoogleDocs_Routines.js
@@ -1,24 +1,34 @@
 export const getDoc = async (selectedDocId) => {
-    const apiToken = sessionStorage.getItem("googleAccessToken");
-    try {
-      const response = await fetch(
-        `https://docs.googleapis.com/v1/documents/${selectedDocId}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${apiToken}`,
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+  const apiToken = sessionStorage.getItem("googleAccessToken");
+  try {
+    const response = await fetch(
+      `https://docs.googleapis.com/v1/documents/${selectedDocId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiToken}`,
+        },
       }
+    );
 
-      const data = await response.json();
-      console.log(data.body.content);
-    } catch (error) {
-      console.error("Error:", error);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-  };
+
+    const data = await response.json();
+    data.body.content.forEach((contentItem, contentIndex) => {
+      if (contentItem.paragraph) {
+        contentItem.paragraph.elements.forEach((element, elementIndex) => {
+          if (element.textRun) {
+            console.log(
+              `Content ${contentIndex}, Element ${elementIndex}: ${element.textRun.content}`
+            );
+          }
+        });
+      }
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
