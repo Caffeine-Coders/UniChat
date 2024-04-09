782a2a1529bdf059f951ae92003eec47c503848d
Dheeraj <dheerajgoud28@gmail.com>
2024-04-08 23:15:38 -0400

    changes

--- Project_SRC/src/app/teacher/classroom/components/chatgpt.js
+++ Project_SRC/src/app/teacher/classroom/components/chatgpt.js
@@ -21,32 +21,22 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 export default function Chatgpt() {
-    let messages1 = []
+    const [messages1, setMessages1] = React.useState([]);
     React.useEffect(() => {
         if (typeof window !== 'undefined') {
-            messages1 = JSON.parse(localStorage.getItem("messages"));
+            const msg1 = JSON.parse(localStorage.getItem("messages"));
+            setMessages1(msg1);
         }
     }, []);
-    
-    console.log("msg1",messages1)
-    const messages = [
-        {
-            sender: 'Dheeraj',
-            message: 'how to change font in react',
-        },
-        {
-            sender: 'ChatGPT',
-            message: 'In React, you can change the font by applying CSS styles to your components. There are several ways to do this: Global Font Change: You can change the font globally by adding a CSS file or style to your project\'s root HTML file. This will apply the font to all elements in your application.Component-Level Font Change: If you want to apply different fonts to specific components, you can include inline styles or CSS classes directly within your React components. Here\'s an example of how you can change the font using inline styles in a React component: import React from \'react\'; const MyComponent = () => { return ( console.log("Hello World") ); }; export default MyComponent;',
-        },
-        {
-            sender: 'Forum',
-            message: 'How to install React',
-        },
-        {
-            sender: 'ChatGPT',
-            message: 'To install React, you\'ll need to set up a new React project. React provides a tool called create-react-app which allows you to quickly set up a new React project with all the necessary dependencies and configuration. Here\'s how you can install React using create-react-app: Install Node.js and npm: React requires Node.js and npm (Node Package Manager) to be installed on your system. You can download and install them from the official Node.js website: Node.js Download. Create a New React Project: Open your terminal or command prompt, and run the following command to install create-react-app globally:',
-        },
-    ]
+
+    console.log("msg1", messages1);
+    let messages=[]
+    messages1.map((msg) => {
+        messages.push({
+            sender: msg[0]=="gpt"? "ChatGPT": msg[0],
+            message: msg[1],
+        });
+    });
   return (
     <>
      <a href='/teacher/classroom'>
